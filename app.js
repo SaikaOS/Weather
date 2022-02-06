22bf0a11da3d3273c5d23a52f7de25f8163e3ac7
SaikaOS <200103235@stu.sdu.edu.kz>
2022-02-06 20:20:25 +0600

    Big update
    Bla bla bla v2

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-let name = document.querySelector('.name') // +
-let temp = document.querySelector('.temp') // +
-let hour = document.querySelector('.hour')
+let name = document.querySelector('.name') 
+let temp = document.querySelector('.temp') 
+let hour = document.querySelector('.hour') 
 let date = document.querySelector('.date')  
-let icon = document.querySelector('.any img') // +
-let desc = document.querySelector('.vars') // +
-const form = document.querySelector('form')
-const btn = document.querySelector('.search_btn')
+let day = document.querySelector('.day')  
+let month = document.querySelector('.month')  
+let year = document.querySelector('.year')  
+let icon = document.querySelector('.icon') 
+let desc = document.querySelector('.vars') 
+let form = document.querySelector('form')
+let btn = document.querySelector('.search_btn')
 let searchInput = document.querySelector('.search_input')
 let cities = document.querySelectorAll('.cities')
-let cloudy = document.querySelector('.cloudy span') // +
-let humidity = document.querySelector('.humidity span') // +
-let wind = document.querySelector('.wind span') // +
+let cloudy = document.querySelector('.cloudy span') 
+let humidity = document.querySelector('.humidity span') 
+let wind = document.querySelector('.wind span') 
 
-
-let cityInput = "Manchester"
+let cityInput = "London"
 
 cities.forEach(city => {
     city.addEventListener('click', (e) => {
-       cityInput = e.target.innerHTML
-
-     fetchWeatherData()
+        cityInput = e.target.innerHTML
+        fetchWeatherData()
+        document.body.style.opacity = '0'
     })
 })
 
@@ -30,7 +32,7 @@ function fetchWeatherData() {
         const namee = data.name
         const tempp = Math.floor(data.main.temp - 273.15)
         const descriptionn = data.weather[0].description 
-      //  const iconn = data.condition.icon.substr("//cdn.weatherapi.com/weather/64x64/".length)
+        const iconn = data.weather[0].icon
         const humidityy = data.main.humidity
         const cloudyy = data.clouds.all
         const windd = data.wind.speed
@@ -42,104 +44,140 @@ function fetchWeatherData() {
         desc.textContent = descriptionn
         wind.textContent = Math.floor(windd) + " km/h"
         cloudy.textContent = Math.floor(cloudyy) + "%"
+        icon.src = `http://openweathermap.org/img/wn/${iconn}@2x.png`
         
-       // icon.src = "./icons/" + iconn
-     //  let curDate = new Date()
-     //  curDate.toLocaleString('en-GB')
+        let d = new Date()
+        d.toLocaleString('en-GB')
+        let localTime = d.getTime()
+        let localOffset = d.getTimezoneOffset() * 60000
+        let utc = localTime + localOffset
+        let atlanta = utc + (1000 * hourr)
+        let nd = new Date(atlanta)
+        let ndd = nd.getHours()
+        let nddMin = nd.getMinutes()
 
-      //let curHour = hourr / 3600
-      /*  let curMin = curDate.getMinutes()
-
-        if(curHour < 10) {
-            curHour = "0" + curHour
+        if(ndd < 10) {
+            ndd = '0' + ndd
         }
 
-        let cityHour = data.timezone / 3600 - curHour
-      //  let cityMin = (cityHour - Math.floor(cityHour)) * 60
+        if (ndd < 5 || ndd > 19) {
+            if (iconn == '02n' || iconn == '03n' || iconn == '04n') {
+                document.body.style.backgroundImage = "url(/images/night/cloudy.jpg)"
+            }
+            else if (iconn == '01n'){
+                document.body.style.backgroundImage = "url(/images/night/clear.jpg)"
+            }
+            else if (iconn == '09n' || iconn == '10n' || iconn == '11n') {
+                document.body.style.backgroundImage = "url(/images/night/rainy.jpg)"
+            }
+            else if (iconn == '13n' || iconn == '50n') {
+                document.body.style.backgroundImage = "url(/images/night/snowy.jpg)"
+            }
+        }
+        else{
+            if (iconn == '02d' || iconn == '03d' || iconn == '04d') {
+                document.body.style.backgroundImage = "url(/images/day/cloudy.jpg)"
+                document.body.style.color = "white"
+            }
+            else if (iconn == '01d'){
+                document.body.style.backgroundImage = "url(/images/day/clear.jpg)"
+                document.body.style.color = "black"
+            }
+            else if (iconn == '09d' || iconn == '10d' || iconn == '11d') {
+                document.body.style.backgroundImage = "url(/images/day/rainy.jpg)"
+                document.body.style.color = "white"
+            }
+            else if (iconn == '13d' || iconn == '50d') {
+                document.body.style.backgroundImage = "url(/images/day/snowy.jpg)"
+                document.body.style.color = "white"
+            }        }
 
-        let cityMin = curMin - Math.abs(data.timezone / 3600) */
 
-       // hour.textContent = Math.floor(cityHour) + ":" + Math.floor(cityMin)
-
-        
-        //calcTime(namee, hourr)
-
-
-
-    let d = new Date()
-    d.toLocaleString('en-GB')
-    let localTime = d.getTime()
-    let localOffset = d.getTimezoneOffset() * 60000
-    let utc = localTime + localOffset
-    let atlanta = utc + (1000 * hourr)
-    let nd = new Date(atlanta)
-    let ndd = nd.getHours()
-    let nddMin = nd.getMinutes()
-
-    if(ndd < 10) {
-        ndd = '0' + ndd
-    }
-
-    if(nddMin < 10) {
+        if(nddMin < 10) {
         nddMin = '0' + nddMin
-    }
+        }
     
-    hour.textContent = ndd + ":" + nddMin
+        hour.textContent = ndd + ":" + nddMin
 
-// Mon Jun 15 2020 17:07:59 GMT-0700
+        const dt = data.dt
+        
+        let dtObj = new Date(dt * 1000)
+
+    function getDayy() {
+        if(dtObj.getDay() == 0) {
+            day.textContent = "Sunday"
+        } else if(dtObj.getDay() == 1) {
+            day.textContent = "Monday"
+        } else if(dtObj.getDay() == 2) {
+            day.textContent = "Tuesday"
+        } else if(dtObj.getDay() == 3) {
+            day.textContent = "Wednesday"
+        } else if(dtObj.getDay() == 4) {
+            day.textContent = "Thursday"
+        } else if(dtObj.getDay() == 5) {
+            day.textContent = "Friday"
+        } else if(dtObj.getDay() == 6) {
+            day.textContent = "Saturday"
+        }
+    }
+
+    function getDatee() {
+        date.textContent = dtObj.getDate()
+    }
+
+    function getMonthh() {
+        if(dtObj.getMonth() == 0) {
+            month.textContent = "January"
+        } else if(dtObj.getMonth() == 1) {
+            month.textContent = "February"
+        } else if(dtObj.getMonth() == 2) {
+            month.textContent = "March"
+        } else if(dtObj.getMonth() == 3) {
+            month.textContent = "April"
+        } else if(dtObj.getMonth() == 4) {
+            month.textContent = "May"
+        } else if(dtObj.getMonth() == 5) {
+            month.textContent = "June"
+        } else if(dtObj.getMonth() == 6) {
+            month.textContent = "July"
+        } else if(dtObj.getMonth() == 7) {
+            month.textContent = "August"
+        } else if(dtObj.getMonth() == 8) {
+            month.textContent = "September"
+        } else if(dtObj.getMonth() == 9) {
+            month.textContent = "Octuber"
+        } else if(dtObj.getMonth() == 10) {
+            month.textContent = "November"
+        } else if(dtObj.getMonth() == 11) {
+            month.textContent = "December"
+        } 
+    }
+
+    function getYearr() {
+       const y1 = dtObj.getFullYear().toString()
+       const y2 = y1.slice(2)
+       year.textContent = y2
+    }
+        getDayy()
+        getDatee()
+        getMonthh()
+        getYearr()
+
+        document.body.style.opacity = '1'
     })
 }
 
-    
-    fetchWeatherData()
+form.addEventListener('submit', e => {
+    if(searchInput.value.length == 0) {
+        alert("Please type in a city name")
+    } else {
+        cityInput = searchInput.value
+        fetchWeatherData()
+        searchInput.value = ''
+        document.body.style.opacity = '0'
+    }
+    e.preventDefault()
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-let curDate = new Date()
-       curDate.toLocaleString('en-GB')
-
-       let curHour = curDate.getHours()
-       let curMin = curDate.getMinutes()
-
-        if(curHour < 10) {
-            curHour = "0" + curHour
-        }
-
-        if(curHour == 0 || curHour > 19) {
-            document.body.style.backgroundImage = "url(/images/night/rainy.jpg)"
-        }
-
-        hour.textContent = curHour + ":" + curMin
-
-*/
-
-
-
-/*
-function calcTime(city, offset) {
-    var d = new Date();
-    var utc = d.getTime() + (offset * 60000);
-    var nd = new Date(utc + (3600000*offset));
-
-    let ndd = nd.getHours()
-    let ndd2 = nd.getMinutes()
-    
-    return ndd + ":" + ndd2
-} */
+fetchWeatherData()
